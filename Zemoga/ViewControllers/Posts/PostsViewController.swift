465d89c441eb1fb3235216d6fef4b023a0f55eb0
Miguel Roncallo <miguel@zipdev.co>
2018-03-12 20:45:56 -0500

    [feat]: Remove storyboard and add main view

---
+++ Zemoga/ViewControllers/Posts/PostsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostsViewController.swift
+//  Zemoga
+//
+//  Created by Miguel Roncallo on 3/12/18.
+//  Copyright © 2018 Miguel Roncallo. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
