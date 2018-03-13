50a32f993bab3e7610ab70782cdbefdb0645d2a1
Miguel Roncallo <miguel@zipdev.co>
2018-03-12 21:37:37 -0500

    [feat]: Set main screen

--- Zemoga/AppDelegate.swift
+++ Zemoga/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let vc = PostsViewController.init()
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
@@ -40,7 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
--- Zemoga/ViewControllers/Posts/PostsViewController.swift
+++ Zemoga/ViewControllers/Posts/PostsViewController.swift
@@ -10,26 +10,22 @@ import UIKit
 
 class PostsViewController: UIViewController {
 
+    //MARK: - Initializers
+    
+    init() {
+        super.init(nibName: String(describing: PostsViewController.self), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        PostsApi.shared.getPosts()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
